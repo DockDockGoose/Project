6a3cc0015456dae584f873a547c1879738ef5bad
Samantha Kosman <skosman@uvic.ca>
2021-01-28 17:17:05 -0800

    Fixed little mistake on schema

--- stocksite/db.sql
+++ stocksite/db.sql
@@ -1,23 +1,21 @@
 /* TODO: 
- *  Need to add UDF or constraint to check that for buying triggers user has enough money in their account 
- *  Or for selling triggers he user has the amount of stock in the account
+ *  Need to add UDF or constraint to check that for buying triggers user has enough money in their account.
+ *  Or for selling triggers he user has the amount of stock in the account.
 */
-
 
 -- Table for all accounts and amount of money available to invest 
 CREATE TABLE accounts (
-    userid varchar(20) PRIMARY KEY,
-    money integer CHECK (money >= 0)
+    userid varchar(50) PRIMARY KEY,
+    funds integer CHECK (funds >= 0)
 );
 
 -- All of the users currents stocks
 CREATE TABLE stocks (
     userid varchar(50) REFERENCES accounts(userid),
     stockSymbol varchar(20),
-    amount integer,
+    amount integer check,
     PRIMARY KEY (userid, stockSymbol)
 );
-
 
 -- Keeps track of set_buy_trigger/set_buy_trigger/cancel_set_buy
 CREATE TABLE buy_triggers (
@@ -35,7 +33,6 @@ CREATE TABLE sell_triggers (
     price integer,
     PRIMARY KEY (userid, stockSymbol, amount)
 );
-
 
 -- Logs every transactions, for dumplog command
 CREATE TABLE logs (
