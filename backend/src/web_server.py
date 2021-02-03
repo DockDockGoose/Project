6b61e0531cf36fd885749cb4e4b882f110dfddd4
damon hunka <hunkadamon@gmail.com>
2021-02-03 12:15:38 -0800

    Flushed out the webserver further

--- backend/src/web_server.py
+++ backend/src/web_server.py
@@ -1,15 +1,27 @@
+""" Webserver class definition. This webserver handles the incoming packets from both the
+    workload generator and web interface. This webserver  """
+
+""" TODO: 
+        - Change to be a DJango type web server (???) 
+        - Make more scalable
+        - Add a method to turn off the server """ 
 
 import socket
-import signal
 import sys
 import time
+import ast
 import threading
+import user_commands
+from user_commands import userCommands
+from threading import Lock
 import datetime as datetime
+
 
 SERV_PORT       = 65432
 SERV_HOST_NAME  = '127.0.0.1'
 
-
+PACKET_SIZE     = 1024
+thread_print_lk = Lock()
 #REFERENCE MATERIALS
 # https://gist.github.com/joncardasis/cc67cfb160fa61a0457d6951eff2aeae
 # https://realpython.com/python-sockets/
@@ -21,6 +33,9 @@ class webServer():
         self.hostname   = hostname
 
     def start(self):
+        """ starts the webserver up. This includes initializing the socket, 
+        binding the socket for recieving, and calling the class listen function"""
+
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         try:
@@ -37,10 +52,13 @@ class webServer():
             self.shutdown()
             sys.exit(1)
 
+        # Call to the listening function to start recieving and decoding incoming packets
         self.listen()
     
     def shutdown(self):
+        """ shutsdown the server connection, and socket. """
         try:
+            self.socket.shutdown(socket.SHUT_RDWR)
             print("~SERVER SHUTDOWN")
             time_now = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
             print("Timestamp: ", time_now)
@@ -49,28 +67,29 @@ class webServer():
             pass
 
     def listen(self):
+        """ Listens to the socket for incoming packets. When a incoming connection
+            occurs a thread will be started to recieve and decode the packet.  """
+
         self.socket.listen(5)
-       
+
         while True:
-            (conn, addy) = self.socket.accept()
+            (conn, address) = self.socket.accept()
             conn.settimeout(60)
-            print("Recieved Connection from: " + str(addy))
-            data = conn.recv(1024)
-            if not data:
-                break;
-            else:
-                print(data)
+            threading.Thread(target=self.handleRequest, args=(conn, address)).start()
 
+    def handleRequest(self, conn, address):
+        """ Listens to the socket for incoming packets. When a incoming connection
+            occurs a thread will be started to recieve and decode the packet.  """
 
-def shutdownServer(sig, unused):
-    server.shutdown()
-    sys.exit(1)
+        data = str(conn.recv(PACKET_SIZE))
+        # decode the packet into a dictionary type
+        # TODO: add client port info so we can send back data. 
+        strData = ast.literal_eval(str(data).strip("b\""))
+        command = strData["command"]
+        userCommands[command](strData)
 
 
 if __name__ == '__main__':
-    signal.signal(signal.SIGINT, shutdownServer)
-
     server = webServer()
-
     server.start()
-    print("Press Ctrl+C to shut down server.")
+    
