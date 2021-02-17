320ee8aa60795ae7cb0b1d8eedeccb5b3d7bc991
damon hunka <hunkadamon@gmail.com>
2021-02-17 14:10:58 -0800

    DAMON: Added setup shell script

---
+++ utils/setup.sh
@@ -0,0 +1,25 @@
+echo "Setting up Enviroment"
+
+cd ~/Desktop
+
+git clone https://github.com/DockDockGoose/Project.git
+
+sudo apt update
+
+sudo apt install -y mongodb
+
+sudo apt install python3-pip
+
+pip3 install pymango
+
+sudo systemctl status mongod
+
+mongo --eval 'db.runCommand({ connectionStatus: 1 })'
+
+mongo
+
+show dbs
+
+use mongodb
+
+show collections
