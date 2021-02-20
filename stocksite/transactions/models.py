344d9a9aa223a1a33087a7691b283b4a4baf0907
DiannaBanana <borinaga33@gmail.com>
2021-02-19 19:01:17 -0800

    adding fields to transactions & serializer

--- stocksite/transactions/models.py
+++ stocksite/transactions/models.py
@@ -1,14 +1,33 @@
 from django.db import models
 
-class Transactions(models.Model):
+class Transaction(models.Model):
+    """
+    Models each transaction on the system.
+    """
     type = models.CharField(max_length=50, blank=True)
-    userId = models.CharField(max_length=50, blank=True)
-    stockSymbol = models.CharField(max_length=50, blank=True)
-    userCommand = models.CharField(max_length=50, blank=True)
+    # UserCommand
     timestamp = models.BigIntegerField(default=0)
-    cryptoKey = models.CharField(max_length=50, blank=True)
-    price = models.FloatField(blank=True)
     server = models.CharField(max_length=50, blank=True)
     transactionNum = models.IntegerField(blank=True)
-    amount = models.FloatField(blank=True)
+    command = models.CharField(max_length=50, blank=True)
+    username = models.CharField(max_length=50, blank=True)
+    stockSymbol = models.CharField(max_length=50, blank=True)
     fileName = models.CharField(max_length=50, blank=True)
+    # Account Transaction
+    action = models.CharField(max_length=50, blank=True)
+    # Funds
+    amount = models.FloatField(blank=True)
+    # Quoteserver
+    price = models.FloatField(blank=True)
+    quoteServerTime = models.BigIntegerField(default=0)
+    cryptoKey = models.CharField(max_length=50, blank=True)
+    # Logs
+    systemEvent = models.CharField(max_length=50, blank=True)
+    errorEvent = models.CharField(max_length=50, blank=True)
+    errorMessage = models.CharField(max_length=50, blank=True)
+    debugEvent = models.CharField(max_length=50, blank=True)
+    debugMessage = models.CharField(max_length=50, blank=True)
+
+    # Used for testing
+    def __str__(self):
+        return self.command
--- stocksite/transactions/serializers.py
+++ stocksite/transactions/serializers.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
-from .models import Transactions
+from .models import Transaction
 
+"""
+more customization can be added, such as:
+
+outputting fields that don’t exist on the model (maybe something like is_new_company, or other data that can be calculated on the backend)
+custom validation logic for when data is sent to the endpoint for any of the fields
+custom logic for creates (POST requests) or updates (PUT or PATCH requests)
+"""
+
+# Serializers define the API representation.
 class TransactionSerializer(serializers.HyperlinkedModelSerializer):
     """
     Serializer used for transaction data representations. Of type HyperlinkedModelSerializer so we can
     use hyperlinked relations. Can also use other Serializers such as for primary key relationships.
     """
     class Meta:
-        model = Transactions
+        model = Transaction
         fields = '__all__'
