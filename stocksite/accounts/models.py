096b9d598b27389dd65fac0901723222b8203097
DiannaBanana <borinaga33@gmail.com>
2021-02-20 22:31:06 -0800

    update transactions app

--- stocksite/accounts/models.py
+++ stocksite/accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Account(AbstractUser):
-    userId = models.CharField(blank=True, max_length=255)
+    userId = models.CharField(blank=False, max_length=255, default='')
     funds = models.FloatField(default=0.00)
     pendingFunds = models.FloatField(default=0.00)
 
--- stocksite/accounts/views.py
+++ stocksite/accounts/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from rest_framework import generics
-
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Account
 from .serializers import AccountSerializer
 
 # TODO: Implement Account view logic
 class AccountListView(generics.ListAPIView):
+    """
+    API endpoint that allows accounts to be viewed or edited.
+    """
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['userId']
