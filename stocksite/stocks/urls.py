86bed62dc881a631f5ab4a06b985921fd43d8976
DiannaBanana <borinaga33@gmail.com>
2021-02-24 16:04:48 -0800

    update stocks urls to include commit sell

--- stocksite/stocks/urls.py
+++ stocksite/stocks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 # from rest_framework import routers
-from .views import BuyView, SellView, QuoteView, CommitBuyView
+from .views import BuyView, SellView, QuoteView, CommitBuyView, CommitSellView
 
 # router = routers.DefaultRouter()
 # router.register(r'stocks', SharesViewSet, basename='stocks')
@@ -8,6 +8,7 @@ from .views import BuyView, SellView, QuoteView, CommitBuyView
 urlpatterns = [
     path('buy/', BuyView.as_view()),
     path('commitbuy/', CommitBuyView.as_view()),
+    path('commitsell/', CommitSellView.as_view()),
     path('sell/', SellView.as_view()),
     path('quote/', QuoteView.as_view()),
     # path('', include(router.urls)),
