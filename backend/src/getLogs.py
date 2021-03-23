475e282b60b4908ca7c3bd4167579a5b70c3ad8b
damon hunka <hunkadamon@gmail.com>
2021-03-23 13:17:52 -0700

    Refactored getLogs

--- backend/src/getLogs.py
+++ backend/src/getLogs.py
@@ -1,8 +1,12 @@
 import pymongo
 import sys
+import queue
+import os
 sys.path.append('..')
 from audit import logXML as log
+from threading import Thread
 
+fileNameQ = queue.Queue()
 
 DB_NAME = 'mongodb'
 DB_PORT = 27017
@@ -13,9 +17,48 @@ CMD_LOG = 'userCommand'
 QUOTE_LOG = 'quoteServer'
 SYSTEM_LOG = 'systemEventType'
 TRANSACT_LOG = 'accountTransaction'
-
 TRANSACT_COLLECT = "transactions"
 
+auditNum = ""
+
+def consumerThread():
+    auditFileName = '../audit/logs/logfile' + auditNum + '.xml'
+    logFile = open(auditFileName, "a")
+    logFile.writelines("<?xml version='1.0' encoding='us-ascii'?>", "<log>")
+
+    while True:
+        try:
+            # set get to throw exception if no packet in 5 seconds
+            filename    = fileNameQ.get(True, 2)
+            with open(filename) as tmpLogFile:
+                fileContent = tmpLogFile.readlines()
+
+                # Take only the logFile content
+                fileContent = fileContent[2:-2]
+
+                # Position logfile at end of file. 
+                logFile.seek(0,2)
+
+                logFile.writelines(fileContent)
+
+            os.remove(auditFileName)
+            fileNameQ.task_done()
+        except queue.Empty:
+            if fileNameQ.empty():
+                break
+
+    logFile.write("</log>")
+    logFile.close()
+    print("Consumer Thread Finished!")
+
+#create consumer thread
+print("Starting Consumer Thread....")
+auditNum = str(input("Enter UserNumber audit number for filename (1 default): ")) or auditNum
+cThread = Thread(target=consumerThread)
+cThread.start()
+print("-----STARTED CONSUMER THREAD-----")
+
+print("Connecting to MongoDB....")
 try:
     client = pymongo.MongoClient(HOST, DB_PORT)
     Database = client[DB_NAME]
@@ -25,13 +68,19 @@ except pymongo.errors.ConnectionFailure as err:
         print(f"ERROR! Could not connect to database {DB_NAME} failed with error: {err}")
         sys.exit(1)
 
-transactions = list(Database[TRANSACT_COLLECT].find())
 
+print("Collecting transactions from the database")
+transactions = list(Database[TRANSACT_COLLECT].find())
+print("-----COLLECTED TRANSACTIONS-----")
+
+
+print("Processing transactions...")
 processing = 0
 for transact in transactions:
     try:
-        print(processing)
+        print("    index: {}".format(processing))
         processing = processing + 1
+
         # Remove the admin user from dumplog commands
         if (transact['user'] == 'admin'):
             transact.pop('user')
@@ -43,10 +92,20 @@ for transact in transactions:
             log.logEvents[QUOTE_LOG](transact)
         elif (transact['logType'] == TRANSACT_LOG):
             log.logEvents[TRANSACT_LOG](transact)
+        
+        #if (processing % 100000):
+        if (processing % 25000):
+            log.prettyPrintLog(fileName=processing)
+            fileNameQ.put('../audit/logs/tmp/logfile' + str(processing) + '.xml')
 
     except KeyError as err:
         print(f'Key Error: {err}\n With log: {transact}')
 
-log.prettyPrintLog()
+log.prettyPrintLog(fileName=processing)
+fileNameQ.put('../audit/logs/tmp/logfile' + str(processing) + '.xml')
+
+print("-----PROCESSED TRANSACTIONS-----")
+
+fileNameQ.join()
 
 print("-----COMPLETED LOG-----")
